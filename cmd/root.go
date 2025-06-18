42b59787fefff023fd14c974bdb797062ca67175
oleksandr-san <oleksandr.v.anosov@gmail.com>
2025-06-18 19:48:21 +0300

    Add log level flag

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,58 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
+
+var logLevel string
+
+func parseLogLevel(logLevel string) zerolog.Level {
+	switch logLevel {
+	case "trace":
+		return zerolog.TraceLevel
+	case "debug":
+		return zerolog.DebugLevel
+	case "info":
+		return zerolog.InfoLevel
+	case "warn":
+		return zerolog.WarnLevel
+	case "error":
+		return zerolog.ErrorLevel
+	default:
+		return zerolog.InfoLevel
+	}
+}
+
+func configureLogger(level zerolog.Level) {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(level)
+
+	if level == zerolog.TraceLevel {
+		zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+			return fmt.Sprintf("%s:%d", file, line)
+		}
+		zerolog.CallerFieldName = "caller"
+		log.Logger = log.Logger.With().Caller().Logger()
+
+		log.Logger = log.Output(zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			TimeFormat: "2006-01-02 15:04:05.000",
+		})
+	} else if level == zerolog.DebugLevel {
+		zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+			return fmt.Sprintf("%s:%d", file, line)
+		}
+		zerolog.CallerFieldName = "caller"
+		log.Logger = log.Logger.With().Caller().Logger()
+	} else {
+		log.Logger = log.Output(os.Stderr)
+	}
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,10 +70,12 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-		log.Info().Msg("This is an info log")
-		log.Debug().Msg("This is a debug log")
+		configureLogger(parseLogLevel(logLevel))
+
+		fmt.Println("Log level is set to:", logLevel)
 		log.Trace().Msg("This is a trace log")
+		log.Debug().Msg("This is a debug log")
+		log.Info().Msg("This is an info log")
 		log.Warn().Msg("This is a warn log")
 		log.Error().Msg("This is an error log")
 	},
@@ -43,11 +91,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.k8s-controller.yaml)")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "Set log level: trace, info, warn, error")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
