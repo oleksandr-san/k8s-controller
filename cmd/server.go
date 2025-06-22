143d94e72532458f00f7810b139a67e18a586186
oleksandr-san <oleksandr.v.anosov@gmail.com>
2025-06-22 20:26:48 +0300

    Add sample informer

--- cmd/server.go
+++ cmd/server.go
@@ -1,15 +1,20 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/oleksandr-san/k8s-controller/pkg/informer"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/valyala/fasthttp"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const (
@@ -45,6 +50,16 @@ var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Start a FastHTTP server",
 	Run: func(cmd *cobra.Command, args []string) {
+		configureLogger(parseLogLevel(viper.GetString("log.level")))
+
+		clientset, err := getServerKubeClient(viper.GetString("kubeconfig"), viper.GetBool("in-cluster"))
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to create Kubernetes client")
+			os.Exit(1)
+		}
+		ctx := context.Background()
+		go informer.StartDeploymentInformer(ctx, clientset)
+
 		handler := func(ctx *fasthttp.RequestCtx) {
 			ctx.SetStatusCode(fasthttp.StatusOK)
 			ctx.SetBodyString("Welcome to the FastHTTP server!")
@@ -66,4 +81,24 @@ func init() {
 	f := serverCmd.Flags()
 	f.Int("port", 8080, "Port to run the server on")
 	viper.BindPFlag("app.port", f.Lookup("port"))
+
+	f.String("kubeconfig", "~/.kube/config", "Path to the kubeconfig file")
+	viper.BindPFlag("kubeconfig", f.Lookup("kubeconfig"))
+
+	f.Bool("in-cluster", false, "Use in-cluster Kubernetes config")
+	viper.BindPFlag("in-cluster", f.Lookup("in-cluster"))
+}
+
+func getServerKubeClient(kubeconfigPath string, inCluster bool) (*kubernetes.Clientset, error) {
+	var config *rest.Config
+	var err error
+	if inCluster {
+		config, err = rest.InClusterConfig()
+	} else {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
 }
