119f1560a1b805e5bbccf50363a9952186da1cbb
oleksandr-san <oleksandr.v.anosov@gmail.com>
2025-06-22 10:29:33 +0300

    Add apply & delete commands

--- cmd/k8sapi.go
+++ cmd/k8sapi.go
@@ -2,14 +2,18 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/dynamic"
 )
 
 var k8sConfigFlags *genericclioptions.ConfigFlags
@@ -62,4 +66,93 @@ func resolveGVR(flags *genericclioptions.ConfigFlags, token string) (schema.Grou
 		return schema.GroupVersionResource{}, fmt.Errorf("unknown resource %q", token)
 	}
 	return full, err
+}
+
+func makeDynamicClient(flags *genericclioptions.ConfigFlags) (*dynamic.DynamicClient, error) {
+	kubeconfigPath := viper.GetString("kubeconfig")
+	flags.KubeConfig = &kubeconfigPath
+	kubeConfig, err := flags.ToRESTConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return dynamicClient, nil
+}
+
+func makeResourceClient(
+	flags *genericclioptions.ConfigFlags,
+	dynamicClient *dynamic.DynamicClient,
+	gvr schema.GroupVersionResource,
+	namespace string,
+) (dynamic.ResourceInterface, error) {
+	if dynamicClient == nil {
+		var err error
+		dynamicClient, err = makeDynamicClient(flags)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if flags.Namespace != nil && *flags.Namespace != "" {
+		namespace = *flags.Namespace
+	}
+
+	if namespace != "" {
+		return dynamicClient.Resource(gvr).Namespace(namespace), nil
+	} else {
+		return dynamicClient.Resource(gvr), nil
+	}
+}
+
+// Buffer size that comfortably fits a typical manifest header.
+const bufSize = 4096
+
+// readObjects takes a list of paths (use "-" for stdin) and returns all parsed K8s objects.
+func readObjects(paths []string) ([]*unstructured.Unstructured, error) {
+	var out []*unstructured.Unstructured
+
+	// default to stdin if no -f was given — same UX as kubectl
+	if len(paths) == 0 {
+		paths = []string{"-"}
+	}
+
+	for _, p := range paths {
+		var r io.ReadCloser
+		switch p {
+		case "-":
+			r = io.NopCloser(os.Stdin)
+		default:
+			f, err := os.Open(p)
+			if err != nil {
+				return nil, fmt.Errorf("open %s: %w", p, err)
+			}
+			defer f.Close()
+			r = f
+		}
+
+		d := yamlutil.NewYAMLOrJSONDecoder(r, bufSize) // streams & splits docs :contentReference[oaicite:4]{index=4}
+		for {
+			u := &unstructured.Unstructured{}
+			err := d.Decode(u)
+			switch {
+			case err == io.EOF: // end of this reader
+				goto nextSource
+			case err != nil:
+				return nil, fmt.Errorf("decode %s: %w", p, err)
+			}
+			// Empty document (possible if file ends with '---\n')
+			if len(u.Object) == 0 {
+				continue
+			}
+			out = append(out, u)
+		}
+	nextSource:
+		r.Close()
+	}
+	return out, nil
 }
--- cmd/k8sapi_list.go
+++ cmd/k8sapi_list.go
@@ -8,11 +8,9 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/scheme"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/printers"
-	"k8s.io/client-go/dynamic"
 )
 
 var k8sListCmd = &cobra.Command{
@@ -24,36 +22,16 @@ var k8sListCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		kubeconfigPath := viper.GetString("kubeconfig")
-		k8sConfigFlags.KubeConfig = &kubeconfigPath
-		kubeConfig, err := k8sConfigFlags.ToRESTConfig()
-		if err != nil {
-			log.Error().Err(err).Msg("failed to read Kubernetes config")
-			os.Exit(1)
-		}
-
 		gvr, err := resolveGVR(k8sConfigFlags, args[0])
 		if err != nil {
 			log.Error().Err(err).Msg("failed to resolve GVR")
 			os.Exit(1)
 		}
 
-		dynamicClient, err := dynamic.NewForConfig(kubeConfig)
+		resourceClient, err := makeResourceClient(k8sConfigFlags, nil, gvr, "")
 		if err != nil {
-			log.Error().Err(err).Msg("Failed to create Kubernetes client")
+			log.Error().Err(err).Msg("failed to create Kubernetes client")
 			os.Exit(1)
-		}
-
-		namespace := ""
-		if k8sConfigFlags.Namespace != nil {
-			namespace = *k8sConfigFlags.Namespace
-		}
-
-		var resourceClient dynamic.ResourceInterface
-		if namespace != "" {
-			resourceClient = dynamicClient.Resource(gvr).Namespace(namespace)
-		} else {
-			resourceClient = dynamicClient.Resource(gvr)
 		}
 
 		resources, err := resourceClient.List(context.Background(), metav1.ListOptions{})
@@ -62,8 +40,8 @@ var k8sListCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		if namespace != "" {
-			fmt.Printf("Found %d %s in '%s' namespace:\n", len(resources.Items), gvr.Resource, namespace)
+		if k8sConfigFlags.Namespace != nil && *k8sConfigFlags.Namespace != "" {
+			fmt.Printf("Found %d %s in '%s' namespace:\n", len(resources.Items), gvr.Resource, *k8sConfigFlags.Namespace)
 		} else {
 			fmt.Printf("Found %d %s in all namespaces:\n", len(resources.Items), gvr.Resource)
 		}
@@ -82,7 +60,7 @@ var k8sListCmd = &cobra.Command{
 				Wide:          true,
 				WithNamespace: true,
 				WithKind:      true,
-				ShowLabels:    true,
+				ShowLabels:    false,
 			})
 		default:
 			log.Error().Msgf("Unknown output format: %s", outputFormat)
